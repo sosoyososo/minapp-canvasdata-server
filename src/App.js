ceb9652addec760935d35be2c1df1c4c6a3e07a1
karsa <wanghongtao114@gmail.com>
2019-12-24 17:20:28 +0800

    first init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,149 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Table, Divider, Tag, Input , Button } from 'antd';
+import {componentsProperties,  convertValueForKey, allTypes} from './const';
+import 'antd/dist/antd.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {    
+  addType(type) { 
+    let list = this.state && this.state.list;       
+    if (!list) list = [];
+    let item = {}
+    item[type] = componentsProperties[type]
+    list.push(item)
+    let propertiesCount = this.getPropertiesMaxCount(list);
+    this.setState({list, propertiesCount})
+  }
+
+  deleteRecord(index) {        
+    let list = this.state && this.state.list;       
+    if (!list) list = [];
+    list = list.filter(function(value, tmpIndex){
+      return index != tmpIndex;
+    });
+    let propertiesCount = this.getPropertiesMaxCount(list);
+    this.setState({list, propertiesCount})
+  }  
+
+  getPropertiesMaxCount(list) {
+    let max = 0
+    list.forEach(item => {
+      Object.keys(item).forEach(key => {
+        let obj = item[key];
+        if (Object.keys(obj).length > max) {
+          max = Object.keys(obj).length;
+        }
+      })      
+    })
+    return max
+  }
+
+  render() {
+    let columns = [{title:'类型', key:"type"}]
+    let propertiesCount = this.state && this.state.propertiesCount;
+    for (var i = 1; i <=  propertiesCount; i ++) {
+      columns.push({title:`属性${i}`, key:`property${i}`})
+    }
+    columns.push({title:'action', key:'action'})
+    columns = columns.map(col => {
+      let item = {
+        title: col.title,
+        key: col.key,
+        dataIndex: col.key,
+      }      
+      if (col.key == "action") {
+        item.render = (key, record, index) => {
+          return (
+            <div>
+              <Button onClick={() => {
+                this.deleteRecord(index)
+              }}>删除</Button>
+            </div>
+          )          
+        }
+      } else if (col.key.startsWith("property")) {        
+        item.render = (key, record, index) => {
+          let obj = record[col.key];          
+          if (!obj) {            
+            return <div />
+          }
+          let name = col.key
+          if (obj) {
+            let keys = Object.keys(obj)            
+            if (keys.length > 0)  {
+              name = keys[0]
+            }            
+          }
+          return (
+            <div>
+              {name}
+              <Input placeholder={name} onChange={(e) => {
+                let obj = this.state.list[index];
+                let key = Object.keys(obj)[0];
+                obj[key][name] = convertValueForKey(name, e.currentTarget.value)
+                this.state.list[index] = obj
+              }} />                 
+            </div>
+          )
+        }
+      }
+      return item;
+    })
+    let datasource = this.state && this.state.list && this.state.list.map(item => {
+      let type = Object.keys(item)[0];
+      let value = item[type]
+      let record = {}
+      record.type = type;
+      Object.keys(value).forEach((k, index) => {
+        let data = {}
+        data[k] = value[k]
+        record[`property${index+1}`] = data
+      })      
+      return record; 
+    })  
+    let content = <div />
+    if (this.state && this.state.content) {
+      content = (
+        <div>
+          {this.state.content}
+        </div>
+      )
+    }
+  
+    return (
+      <div>
+        <Table   
+          title={() => {
+            let items = allTypes.map(type => {
+              return (
+                <Button className="default-margin" onClick={() => {
+                  this.addType(type)            
+                }}>{type}</Button>
+              )
+            });
+            return (
+              <div className="default-padding">
+                {items}
+              </div>
+            )
+          }}
+          columns={columns} 
+          dataSource={datasource}
+          footer={() => {
+            return (
+              <div>
+                <Button onClick={() => {                                    
+                  this.setState({content: JSON.stringify(this.state.list)})
+                }}>内容</Button>
+              </div>
+            )            
+          }}
+        />
+        {content}        
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/const.js
@@ -0,0 +1,86 @@
+export const componentsProperties = {
+  text: {
+    text: "",
+    x: 0,
+    y: 0,
+    fontSize: 0,
+    fontColor: 'black',
+    bold: false,
+    wrap: false,
+    maxWidth: 0,
+    lineSpace: 0
+  },
+  line: {
+    x: 0,
+    y: 0,
+    w: 0,
+    h: 0,
+    color: 'black',
+    lineWidth: 1
+  },
+  rect: {
+    x: 0,
+    y: 0,
+    w: 0,
+    h: 0,
+    r: 0,
+    topLeft: false,
+    topRight: false,
+    bottomLeft: false,
+    bottomRight: false,
+    color: 'black',
+    lineWidth: 0
+  },
+  image: {
+    url: '',
+    x: 0,
+    y: 0,
+    w: 0,
+    h: 0,
+    r: 0,
+    color: 'black',
+    lineWidth: 0
+  }
+}
+
+export const allTypes = getAllTypes()
+
+export function getAllTypes() {
+  return Object.keys(componentsProperties)
+}
+
+export function getAllPropertiesForType(type) {
+  let properties = []
+  let obj = componentsProperties[type];
+    Object.keys(obj).forEach(key => {            
+      if (!properties.includes(key)) {        
+        properties.push(key)
+      }
+    })
+  return properties;
+}
+
+export function getAllProperties() {
+  let properties = []
+  Object.keys(componentsProperties).forEach(key => {
+    let obj = componentsProperties[key];
+    Object.keys(obj).forEach(key => {            
+      if (!properties.includes(key)) {        
+        properties.push(key)
+      }
+    })
+  })
+  return properties;
+}
+
+export function convertValueForKey(key, value) {  
+  let numKeyList = ['x','y','h','r','fontSize','maxWidth', 'lineSpace', 'lineWidth']
+  let boolKeyList = ["bold", "wrap","topLeft", "topRight", "bottomLeft", "bottomRight"]
+  if (numKeyList.includes(key)) {
+    return Number.parseInt(value)
+  } else if (boolKeyList.includes(key)) {
+    return value.toLocaleLowerCase() == 'true'
+  } {
+    return value
+  }
+}
